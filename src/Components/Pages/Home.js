902e72b8cc23904cb83a4e5dad38e143c0418ae3
jpeek21 <jonathan.peek21@gmail.com>
2025-05-17 00:32:18 -0700

    Update Home.js

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -4,6 +4,7 @@ import VerticalTimeline from './VerticalTimeline';
 import myFace from '../../Assets/my_face2.png';
 import background from '../../Assets/home_background.png';
 import { Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function Home() {
   return (
@@ -16,18 +17,17 @@ function Home() {
         <div className="profile-card">
           <img src={myFace} alt="Jonathan Peek" className="profile-pic" />
           <div className="profile-text">
-  <h1>Jonathan Peek</h1>
-  <p className="indented">
-    Hi, thanks for checking out my portfolio! 
-  </p>
-  <p className="indented">
-    I am a second-year Mechanical Engineering student at UC Irvine, aiming to specialize in control systems. My research emphasis is on electromagnetic systems and I am always looking for more opportunities to learn.
-  </p>
-  <p className="indented">
-    If you have any questions while you are here, shoot me a message!
-  </p>
-</div>
-
+            <h1>Jonathan Peek</h1>
+            <p className="indented">
+              Hi, thanks for checking out my portfolio!
+            </p>
+            <p className="indented">
+              I am a second-year Mechanical Engineering student at UC Irvine, aiming to specialize in control systems. My research emphasis is on electromagnetic systems and I am always looking for more opportunities to learn.
+            </p>
+            <p className="indented">
+              If you have any questions while you are here, shoot me a message!
+            </p>
+          </div>
         </div>
         <div className="fade-overlay" />
       </div>
@@ -40,8 +40,8 @@ function Home() {
         <h2>Want to know what else I've been up to?</h2>
         <div className="cta-buttons">
           <Link to="/Projects" className="cta-button">Projects</Link>
-<Link to="/Resume" className="cta-button">Resume</Link>
-<Link to="/Contact" className="cta-button">Contact</Link>
+          <Link to="/Resume" className="cta-button">Resume</Link>
+          <Link to="/Contact" className="cta-button">Contact</Link>
         </div>
       </div>
     </div>
